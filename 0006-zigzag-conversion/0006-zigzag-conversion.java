63c1f6b46103c38283452daa76303a5076acf518
soundarya635 <soundaryag635@gmail.com>
2026-06-15 15:17:38 +0530

    Time: 8 ms (18.97%), Space: 46.9 MB (29.69%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,40 +1,37 @@
 class Solution {
-    public String convert(String s, int n) {
-        if(n>=s.length()|| n==1) return s;
-        int i=0,j=0,col=0;
-        int c=0;
-        int l=s.length();
-        char ans[][]=new char[n][l];
-        while(i<n|| j<l){
-            int f=0;
-            if(i==0){
-                while(i<n){
-                    if(c>=l) {
-                        f=1;
-                        break;
-                    }
-                    ans[i][j]=s.charAt(c);
-                    c++;i++;
-                }i--;
-                i--;j++;
-            }
-            while(i!=0){
-                if(c>=l) {
-                    f=1;break;
+    public String convert(String s, int numRows) {
+        if(numRows==1) return s;
+        HashMap<Integer,StringBuilder> map=new HashMap<>();
+        StringBuilder ans=new StringBuilder();
+        for(int i=0;i<numRows;i++){
+            map.put(i,new StringBuilder());
+        }
+        int k=0,m=numRows-1;
+        boolean up=true;
+        for(char ch:s.toCharArray()){
+            map.get(k).append(ch);
+            if(up){
+                
+                if(k==numRows-1){
+                    k--;
+                    up=false;
+                    continue;
                 }
-                ans[i][j]=s.charAt(c);
-                c++;i--;j++;
+                k++;
             }
-            if(f==1){
-                col=j;break;
+            else{
+                
+                if(k==0){
+                    k++;
+                    up=true;
+                    continue;
+                }
+                k--;
             }
         }
-        StringBuilder t=new StringBuilder();
-        for(int k=0;k<n;k++){
-            for(int r=0;r<col;r++){
-                if(ans[k][r]!='\u0000') t.append(ans[k][r]);
-            }
+        for(int i=0;i<numRows;i++){
+            ans.append(map.get(i));
         }
-        return t.toString();
+        return ans.toString();
     }
 }

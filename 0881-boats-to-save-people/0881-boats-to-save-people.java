4215b78ba613e8b838bfb539f27aacc41f8323cd
soundarya635 <soundaryag635@gmail.com>
2026-07-07 21:31:55 +0530

    Time: 19 ms (98.44%), Space: 56.6 MB (37.82%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int numRescueBoats(int[] p, int limit) {
+        Arrays.sort(p);
+        int l=0,r=p.length-1,c=0;
+        while(l<=r){
+            if(p[l]+p[r]<=limit){
+                l++;
+            }r--;c++;
+        }
+        return c;
+    }
+}

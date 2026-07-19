a2b59c6cb2f9bbf81c16f899cc316c83e9ef1086
soundarya635 <soundaryag635@gmail.com>
2026-07-19 21:55:08 +0530

    Time: 1 ms (86.48%), Space: 42.7 MB (86.29%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,34 +1,31 @@
 class Solution {
 
     public String decodeString(String s) {
-        return decode(s);
-    }
+        Stack<Integer> c=new Stack<>();
+        Stack<StringBuilder> st=new Stack<>();
 
-    int i = 0;
+        StringBuilder cur=new StringBuilder();
+        int num =0;
+        for(int i=0;i<s.length();i++){
+            if(Character.isDigit(s.charAt(i))){
+                num=(num*10) + Integer.parseInt(s.charAt(i)+"");
+            }else if(s.charAt(i)=='['){
+                c.push(num);
+                st.push(cur);
+                num=0;
+                cur=new StringBuilder();
+            }else if(s.charAt(i)==']'){
+                int repeat=c.pop();
+                StringBuilder prev=st.pop();
 
-    public String decode(String s) {
-        StringBuilder st = new StringBuilder();
-
-        while (i < s.length() && s.charAt(i) != ']') {
-            if (Character.isLetter(s.charAt(i)))
-                st.append(s.charAt(i++));
-            else {
-                int num = 0;
-                    while(Character.isDigit(s.charAt(i))) {
-                        num = (num * 10) + Integer.parseInt(s.charAt(i) + "");
-                        i++;
-                    }
-                    
-                    i++;
-
-                    String inner=decode(s);
-                    i++ ;
-                    for(int k=0;k<num;k++) st.append(inner);
-
+                for(int k=0;k<repeat;k++){
+                    prev.append(cur);
                 }
-            
+                cur=prev;
+            }else cur.append(s.charAt(i));
         }
-        return st.toString();
-
+        return cur.toString();
     }
+
+   
 }

e1f0151bf3b61348c7ecabc88df9cd18ed7e8613
soundarya635 <soundaryag635@gmail.com>
2026-07-31 21:30:31 +0530

    Time: 0 ms (100%), Space: 42 MB (55.16%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,12 +1,14 @@
 class Solution {
-     int dp[];
+     
     public int climbStairs(int n) {
-        dp=new int[n+1];
-        return recur(n);
+        int dp[]=new int[n+1];
+        if(n==1 || n==2) return n;
+        dp[0]=0;dp[1]=1;dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+        
     }
-    public int recur(int n){
-        if(n==1 || n==2 ) return n;
-        if(dp[n]!=0) return dp[n];
-        return dp[n]=recur(n-1) + recur(n-2);
-    }
+     
 }

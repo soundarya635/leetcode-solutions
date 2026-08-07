a0b89bda9b3cb6d6342a785d8cb69a30e15f9707
soundarya635 <soundaryag635@gmail.com>
2026-08-07 23:03:47 +0530

    Time: 4 ms (62.25%), Space: 50.1 MB (86.01%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -3,13 +3,23 @@ class Solution {
         int n=g.length;
         int m=g[0].length;
         int dp[][]=new int[n][m];
-        for(int i=0;i<n;i++) Arrays.fill(dp[i],-1);
-        return recur(dp,g,n-1,m-1);
+        dp[0][0]=g[0][0];
+        for(int i=1;i<n;i++) dp[i][0]=dp[i-1][0]+g[i][0];
+        for(int i=1;i<m;i++) dp[0][i]=dp[0][i-1]+g[0][i];
+
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                dp[i][j]=g[i][j] + Math.min(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[n-1][m-1];
+        // for(int i=0;i<n;i++) Arrays.fill(dp[i],-1);
+        // return recur(dp,g,n-1,m-1);
     }
-    public int recur(int dp[][],int g[][],int i,int j){
-        if(i<0 || j< 0) return Integer.MAX_VALUE;
-        if(i==0 && j==0) return g[i][j];
-        if(dp[i][j] != -1) return dp[i][j];
-        return dp[i][j]=g[i][j] + Math.min(recur(dp,g,i-1,j),recur(dp,g,i,j-1));
-    }
+    // public int recur(int dp[][],int g[][],int i,int j){
+    //     if(i<0 || j< 0) return Integer.MAX_VALUE;
+    //     if(i==0 && j==0) return g[i][j];
+    //     if(dp[i][j] != -1) return dp[i][j];
+    //     return dp[i][j]=g[i][j] + Math.min(recur(dp,g,i-1,j),recur(dp,g,i,j-1));
+    // }
 }

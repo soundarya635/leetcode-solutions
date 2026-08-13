0a33de9bd8d690741475b2fc93d63eb4cd5f8fff
soundarya635 <soundaryag635@gmail.com>
2026-08-13 21:13:14 +0530

    Time: 6 ms (8.71%), Space: 47.2 MB (53.08%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,8 +1,23 @@
 class Solution {
     public int minDistance(String w1, String w2) {
-        int dp[][]=new int[w1.length()][w2.length()];
-        for(int i=0;i<w1.length();i++) Arrays.fill(dp[i],-1);
-        return recur(w1,w2,dp,w1.length()-1,w2.length()-1);
+        int dp[][]=new int[w1.length()+1][w2.length()+1];
+        if(w1.length()==0) return w2.length();
+        if(w2.length()==0) return w1.length();
+        for(int i=0;i<=w1.length();i++) dp[i][0]=i;
+        for(int i=0;i<=w2.length();i++) dp[0][i]=i;
+
+        for(int i=1;i<=w1.length();i++){
+            for(int j=1;j<=w2.length();j++){
+                if(w1.charAt(i-1)==w2.charAt(j-1)) dp[i][j]=dp[i-1][j-1];
+                else{
+                    dp[i][j]=1+Math.min(dp[i-1][j],Math.min(dp[i][j-1],dp[i-1][j-1]));
+                }
+            }
+        }
+        return dp[w1.length()][w2.length()];
+
+        // for(int i=0;i<w1.length();i++) Arrays.fill(dp[i],-1);
+        // return recur(w1,w2,dp,w1.length()-1,w2.length()-1);
     }
     public int recur(String s1,String s2,int[][] dp,int i,int j){
         

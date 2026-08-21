1e98e865533f0a681f069ed0609ce5fe2dcfa4bb
Soundarya G <soundaryag635@gmail.com>
2026-08-21 16:09:17 +0530

    Time: 1 ms (100%), Space: 42.6 MB (63.8%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        long low=1,high=n,ans=0;
+        while(low<=high){
+            long mid=low+(high-low)/2;
+            long x=(mid*(mid+1))/2;
+            if(x<=n){
+                ans=mid;
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return (int) ans;
+
+    }
+}
